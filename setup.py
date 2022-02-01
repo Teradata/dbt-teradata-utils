a1b7dbbffe390ee2ad4df1189e71c46d23fd3503
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2022-02-01 13:31:52 -0600

    provided dependency setup

---
+++ setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import os
+import sys
+
+from setuptools import setup
+
+
+if sys.version_info < (3, 7) or sys.version_info >= (3, 10):
+    print('Error: dbt-teradata does not support this version of Python.')
+    print('Please install Python 3.7 or higher but less than 3.10.')
+    sys.exit(1)
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+
+package_name = "dbt-teradata-utils"
+package_version = "0.0.1a"
+description = """The Teradata utils for dbt (data build tool)"""
+
+
+setup(
+    name=package_name,
+    version=package_version,
+
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author="Teradata Corporation",
+    author_email="developers@teradata.com",
+    url="https://github.com/Teradata/dbt-teradata-utils",
+    package_data={
+        'dbt.include.teradata': [
+            'macros/*.sql'
+        ],
+    },
+    install_requires=[
+        "dbt-core==1.0.1",
+        "teradatasql>=16.20.0.0",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires=">=3.7,<3.10",
+)
