7b18e6d7f58f1c5083e6e188cbbda29202ba5059
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2022-02-03 07:23:19 -0600

    explained why datediff needs custom impl

--- macros/datediff.sql
+++ macros/datediff.sql
@@ -1,3 +1,6 @@
+{# This macro needed to be overridden because the original implementation #}
+{# used 'datediff' function that is not supported on Vantage #}
+
 {% macro teradata__datediff(first_date, second_date, datepart) %}
   ({{ second_date }} - {{ first_date }}) {{ datepart }}(4)
 {% endmacro %}
