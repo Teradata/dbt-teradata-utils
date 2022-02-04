1091fcf2bd5a0f8c553f595ff7d72fcf7be1cfe4
Adam Tworkiewicz <adam.tworkiewicz@teradata.com>
2022-02-04 14:48:34 -0600

    added support for literals in datediff

--- macros/datediff.sql
+++ macros/datediff.sql
@@ -2,5 +2,15 @@
 {# used 'datediff' function that is not supported on Vantage #}
 
 {% macro teradata__datediff(first_date, second_date, datepart) %}
+
+  {# we need to cast literals to date #}
+  {% if first_date.0 == "'" %}
+    {% set first_date = "cast(" + first_date + " as date)" %}
+  {% endif %}
+
+  {% if second_date.0 == "'" %}
+    {% set second_date = "cast(" + second_date + " as date)" %}
+  {% endif %}
+
   ({{ second_date }} - {{ first_date }}) {{ datepart }}(4)
 {% endmacro %}
