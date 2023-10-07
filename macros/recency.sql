6291437fa0f0533a466a0b50c6c8f17ba85790a5
Septimiu Turcu <septimiu.turcu@teradata.com>
2023-10-07 19:52:35 +0200

    Update test_recency macro

--- macros/recency.sql
+++ macros/recency.sql
@@ -1,20 +1,38 @@
 {# Overriding because the original implementation used 'as threshold' but 'threshold' is a reserved word in Teradata #}
-{% macro teradata__test_recency(model, field, datepart, interval) %}
+{% macro teradata__test_recency(model, field, datepart, interval, ignore_time_component, group_by_columns) %}
 
-{% set threshold = dbt_utils.dateadd(datepart, interval * -1, dbt_utils.current_timestamp()) %}
+{% set threshold = 'cast(' ~ dbt.dateadd(datepart, interval * -1, dbt.current_timestamp()) ~ ' as ' ~ ('date' if ignore_time_component else dbt.type_timestamp()) ~ ')'  %}
 
-WITH recency AS (
+{% if group_by_columns|length() > 0 %}
+  {% set select_gb_cols = group_by_columns|join(' ,') + ', ' %}
+  {% set groupby_gb_cols = 'group by ' + group_by_columns|join(',') %}
+{% endif %}
 
-    SELECT max({{field}}) AS most_recent
-    FROM {{ model }}
+
+with recency as (
+
+    select
+
+      {{ select_gb_cols }}
+      {% if ignore_time_component %}
+        cast(max({{ field }}) as date) as most_recent
+      {%- else %}
+        max({{ field }}) as most_recent
+      {%- endif %}
+
+    from {{ model }}
+
+    {{ groupby_gb_cols }}
 
 )
 
-SELECT
-    most_recent,
-    {{ threshold }} AS _threshold
+select
 
-FROM recency
-WHERE most_recent < {{ threshold }}
+    {{ select_gb_cols }}
+    most_recent,
+    {{ threshold }} as _threshold
+
+from recency
+where most_recent < {{ threshold }}
 
 {% endmacro %}
