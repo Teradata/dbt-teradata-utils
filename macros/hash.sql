7b1eb0e7da0949e84f8c51d50fb9812d97688fa2
Septimiu Turcu <septimiu.turcu@teradata.com>
2023-10-08 17:54:45 +0200

    Remove macros that are not hosted in dbt-utils

--- macros/hash.sql
+++
@@ -1,8 +0,0 @@
-
-{# The original implementation used 'md5' function which is not available in Teradata #}
-{# This implementation requires that a custom UDF is installed in 'GLOBAL_FUNCTIONS' database #}
-{# See README for details. #}
-
-{% macro teradata__hash(field) -%}
-    GLOBAL_FUNCTIONS.hash_md5(cast({{field}} AS {{dbt_utils.type_string()}}))
-{%- endmacro %}
