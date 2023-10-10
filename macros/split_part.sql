890d712d01c0a560b9862d19c234eeda874f58b0
Septimiu Turcu <septimiu.turcu@teradata.com>
2023-10-10 08:59:16 +0200

    Update split_part macro

--- macros/split_part.sql
+++ macros/split_part.sql
@@ -1,6 +1,6 @@
 {# The function used in the original macro ('split_part') is called 'strtok' in Teradata #}
 
-{% macro default__split_part(string_text, delimiter_text, part_number) %}
+{% macro teradata__split_part(string_text, delimiter_text, part_number) %}
   {# if delimiter_text is a single character literal #}
   {% if delimiter_text|length == 3 and delimiter_text.0  =="'" and delimiter_text.2  == "'" %}
     strtok(
