1f6686dd83511eb72c741377e77cce2935238ed5
vs255034 <vs255034@teradata.com>
2025-11-03 15:17:56 +0530

    feature: added dbt-date macros support used for dbt-expectations

---
+++ macros/dbt_date_macros/get_base_dates.sql
@@ -0,0 +1,42 @@
+{% macro teradata__get_base_dates(start_date, end_date, n_dateparts, datepart) %}
+
+    {%- if start_date and end_date -%}
+        {%- set start_date = (
+            "cast('" ~ start_date ~ "' as " ~ dbt.type_timestamp() ~ ")"
+        ) -%}
+        {%- set end_date = (
+            "cast('" ~ end_date ~ "' as " ~ dbt.type_timestamp() ~ ")"
+        ) -%}
+
+    {%- elif n_dateparts and datepart -%}
+
+        {%- set start_date -%} 
+            cast(
+            {{ dbt.dateadd(
+            datepart, -1 * n_dateparts, dbt_date.today())}} as date)
+        {%- endset -%}
+        {%- set end_date = dbt_date.tomorrow() -%}
+    {%- endif -%}
+
+    {% set intervals = dbt_utils.get_intervals_between(start_date, end_date, datepart) %}
+
+    with
+        date_spine as (
+
+            SELECT date_val as date_{{datepart}}
+
+            FROM (
+
+                SELECT {{ dbt.dateadd(datepart, 'row_number() over (order by generated_number) - 1', start_date) }} as date_val
+
+                FROM ({{ dbt_utils.generate_series(intervals) }}) as gs
+
+            ) as sub
+
+            WHERE date_val <= {{ end_date }}
+
+        )
+    select
+        cast(d.date_{{ datepart }} as {{ dbt.type_timestamp() }}) as date_{{ datepart }}
+    from date_spine d
+{% endmacro %}
